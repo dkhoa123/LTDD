d9449cf5e160057ff2c00f692a78041d01158946
Thanh Quy <quyht2908@gmail.com>
2025-03-19 20:21:53 +0700

    Complete info_layout.kt and sign_up_layout.kt

--- app/src/main/java/com/example/appchat_tbdd/ui_layout/info_layout.kt
+++ app/src/main/java/com/example/appchat_tbdd/ui_layout/info_layout.kt
@@ -2,7 +2,6 @@ package com.example.appchat_tbdd.ui_layout
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.runtime.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,31 +12,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,10 +40,13 @@ import com.example.appchat_tbdd.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun signInLayout(navController: NavController? = null, modifier: Modifier = Modifier){
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    var passwordVisible by remember { mutableStateOf(false) } // Kiểm soát hiển thị mật khẩu
+fun Info_layout(navController: NavController? = null, modifier: Modifier = Modifier){
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
+    var age by remember { mutableStateOf("") }
+    var address by remember { mutableStateOf("") }
+    var sex by remember { mutableStateOf("") }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -63,24 +59,24 @@ fun signInLayout(navController: NavController? = null, modifier: Modifier = Modi
                 .blur(4.dp)
         ) {
             drawCircle(
-                color = Color(0xFF0077B6),
-                radius = 260f,
-                center = center.copy(x = size.width * 0.20f, y = size.height * 0.05f)
-            )
-            drawCircle(
                 color = Color(0xFF0096C7),
-                radius = 400f,
-                center = center.copy(x = size.width * 0.7f, y = size.height * 0.05f)
+                radius = 260f,
+                center = center.copy(x = size.width * 0.8f, y = size.height * 0.05f)
+            )
+            drawCircle(
+                color = Color(0xFF0077B6),
+                radius = 500f,
+                center = center.copy(x = size.width * 0.2f, y = size.height * 0)
             )
             drawCircle(
                 color = Color(0xFF0077B6),
                 radius = 260f,
-                center = center.copy(x = size.width * 0.1f, y = size.height * 0.95f)
+                center = center.copy(x = size.width * 0.9f, y = size.height * 0.95f)
             )
             drawCircle(
                 color = Color(0xFF0077B6),
                 radius = 100f,
-                center = center.copy(x = size.width * 0.4f, y = size.height * 0.85f)
+                center = center.copy(x = size.width * 0.25f, y = size.height * 0.87f)
             )
         }
         Column (
@@ -96,23 +92,22 @@ fun signInLayout(navController: NavController? = null, modifier: Modifier = Modi
                 Text(
                     text = "Sign In",
                     fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
-                    textDecoration = TextDecoration.Underline,
+                    color = Color.Gray,
                     modifier = Modifier
                         .padding(end = 25.dp)
                 )
                 Text(
                     text = "Sign Up",
                     fontSize = 24.sp,
-                    color = Color.Gray,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    textDecoration = TextDecoration.Underline,
                 )
             }
-            // Ô nhập Email hoặc Số điện thoại
             OutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                label = { Text("Phone/ Email") },
-                leadingIcon = { Icon(Icons.Default.Email, contentDescription = "Email Icon") },
+                value = firstName,
+                onValueChange = { firstName = it },
+                label = { Text("First Name") },
                 singleLine = true,
                 shape = RoundedCornerShape(20.dp), // Bo góc
                 //Jetpack Compose Material3
@@ -124,74 +119,89 @@ fun signInLayout(navController: NavController? = null, modifier: Modifier = Modi
 
             Spacer(modifier = Modifier.height(12.dp))
 
-            // Ô nhập Mật khẩu
             OutlinedTextField(
-                value = password,
-                onValueChange = { password = it },
-                label = { Text("Password") },
-                leadingIcon = { Icon(Icons.Default.Lock, contentDescription = "Password Icon") },
-                trailingIcon = {
-                    val icon = if (passwordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff
-                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
-                        Icon(icon, contentDescription = "Toggle Password Visibility")
-                    }
-                },
+                value = lastName,
+                onValueChange = { lastName = it },
+                label = { Text("Last Name") },
                 singleLine = true,
-                shape = RoundedCornerShape(20.dp),
+                shape = RoundedCornerShape(20.dp), // Bo góc
                 //Jetpack Compose Material3
                 colors = TextFieldDefaults.outlinedTextFieldColors(
                     containerColor = Color.White
                 ),
-                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
                 modifier = Modifier.fillMaxWidth(0.9f)
             )
-            Row (
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 18.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Row (
-                    modifier = Modifier.padding(end = 75.dp)
-                ){
-                    Image(
-                        painter = painterResource(id = R.drawable.icon_check_circle),
-                        contentDescription = "Checkbox",
-                        modifier = Modifier
-                            .padding(end = 4.dp)
-                    )
 
-                    Text(
-                        text = "Remember me",
-                        fontSize = 16.sp,
-                    )
-                }
+            Spacer(modifier = Modifier.height(12.dp))
 
-                Text(
-                    text = "Forgot password",
-                    fontSize = 16.sp,
-                    color = Color(0xFF03045E)
-                )
-            }
+            OutlinedTextField(
+                value = age,
+                onValueChange = { age = it },
+                label = { Text("Age") },
+                singleLine = true,
+                shape = RoundedCornerShape(20.dp), // Bo góc
+                //Jetpack Compose Material3
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    containerColor = Color.White
+                ),
+                modifier = Modifier.fillMaxWidth(0.9f)
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            OutlinedTextField(
+                value = address,
+                onValueChange = { address = it },
+                label = { Text("Address") },
+                singleLine = true,
+                shape = RoundedCornerShape(20.dp), // Bo góc
+                //Jetpack Compose Material3
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    containerColor = Color.White
+                ),
+                modifier = Modifier.fillMaxWidth(0.9f)
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            OutlinedTextField(
+                value = sex ,
+                onValueChange = { sex = it },
+                label = { Text("Sex") },
+                singleLine = true,
+                shape = RoundedCornerShape(20.dp), // Bo góc
+                //Jetpack Compose Material3
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    containerColor = Color.White
+                ),
+                modifier = Modifier.fillMaxWidth(0.9f)
+            )
+
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Nút Đăng nhập
+            // Nút Chuyển tiếp
             Button(
                 onClick = { /* Xử lý đăng nhập */ },
                 modifier = Modifier
-                    .fillMaxWidth(0.6f)
-                    .height(50.dp),
-                shape = RoundedCornerShape(25.dp),
-                colors = ButtonDefaults.buttonColors(Color(0xFF023E8A)) // Màu xanh dương đậm
+                    .fillMaxWidth(0.2f)
+                    .height(60.dp),
+                colors = ButtonDefaults.buttonColors(Color.Gray),
+                shape = RoundedCornerShape(25.dp)
             ) {
-                Text(text = "Sign In", fontSize = 18.sp, color = Color.White)
+                Image(
+                    painter = painterResource(id = R.drawable.image_button_next),
+                    contentDescription = "Next",
+                    modifier = Modifier
+                        .size(50.dp)
+                        .fillMaxWidth(),
+                )
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview (showBackground = true)
 @Composable
-fun previewSignInLayout (){
-    com.example.appchat_tbdd.ui_layout.Sign_up_layout()
+fun Info_layout_preview (){
+    Info_layout()
 }
--- app/src/main/java/com/example/appchat_tbdd/ui_layout/sign_up_layout.kt
+++ app/src/main/java/com/example/appchat_tbdd/ui_layout/sign_up_layout.kt
@@ -2,7 +2,6 @@ package com.example.appchat_tbdd.ui_layout
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.runtime.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
@@ -28,11 +28,14 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -43,11 +46,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.test.espresso.core.internal.deps.dagger.Component
 import com.example.appchat_tbdd.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun signInLayout(navController: NavController? = null, modifier: Modifier = Modifier){
+fun Sign_up_layout (navController: NavController? = null, modifier: Modifier = Modifier){
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var passwordVisible by remember { mutableStateOf(false) } // Kiểm soát hiển thị mật khẩu
@@ -60,27 +64,27 @@ fun signInLayout(navController: NavController? = null, modifier: Modifier = Modi
         Canvas(
             modifier = Modifier
                 .fillMaxSize()
-                .blur(4.dp)
+//                .blur(4.dp)
         ) {
             drawCircle(
-                color = Color(0xFF0077B6),
-                radius = 260f,
-                center = center.copy(x = size.width * 0.20f, y = size.height * 0.05f)
-            )
-            drawCircle(
                 color = Color(0xFF0096C7),
-                radius = 400f,
-                center = center.copy(x = size.width * 0.7f, y = size.height * 0.05f)
+                radius = 260f,
+                center = center.copy(x = size.width * 0.8f, y = size.height * 0.05f)
+            )
+            drawCircle(
+                color = Color(0xFF0077B6),
+                radius = 500f,
+                center = center.copy(x = size.width * 0.2f, y = size.height * 0)
             )
             drawCircle(
                 color = Color(0xFF0077B6),
                 radius = 260f,
-                center = center.copy(x = size.width * 0.1f, y = size.height * 0.95f)
+                center = center.copy(x = size.width * 0.9f, y = size.height * 0.95f)
             )
             drawCircle(
                 color = Color(0xFF0077B6),
                 radius = 100f,
-                center = center.copy(x = size.width * 0.4f, y = size.height * 0.85f)
+                center = center.copy(x = size.width * 0.25f, y = size.height * 0.87f)
             )
         }
         Column (
@@ -96,22 +100,24 @@ fun signInLayout(navController: NavController? = null, modifier: Modifier = Modi
                 Text(
                     text = "Sign In",
                     fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
-                    textDecoration = TextDecoration.Underline,
+                    color = Color.Gray,
                     modifier = Modifier
                         .padding(end = 25.dp)
                 )
                 Text(
                     text = "Sign Up",
                     fontSize = 24.sp,
-                    color = Color.Gray,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    textDecoration = TextDecoration.Underline,
                 )
             }
+
             // Ô nhập Email hoặc Số điện thoại
             OutlinedTextField(
                 value = email,
                 onValueChange = { email = it },
-                label = { Text("Phone/ Email") },
+                label = { Text("Phone number or email") },
                 leadingIcon = { Icon(Icons.Default.Email, contentDescription = "Email Icon") },
                 singleLine = true,
                 shape = RoundedCornerShape(20.dp), // Bo góc
@@ -124,18 +130,12 @@ fun signInLayout(navController: NavController? = null, modifier: Modifier = Modi
 
             Spacer(modifier = Modifier.height(12.dp))
 
-            // Ô nhập Mật khẩu
+            // Ô nhập Mật khẩu (tạo mật khẩu)
             OutlinedTextField(
                 value = password,
                 onValueChange = { password = it },
                 label = { Text("Password") },
                 leadingIcon = { Icon(Icons.Default.Lock, contentDescription = "Password Icon") },
-                trailingIcon = {
-                    val icon = if (passwordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff
-                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
-                        Icon(icon, contentDescription = "Toggle Password Visibility")
-                    }
-                },
                 singleLine = true,
                 shape = RoundedCornerShape(20.dp),
                 //Jetpack Compose Material3
@@ -145,53 +145,63 @@ fun signInLayout(navController: NavController? = null, modifier: Modifier = Modi
                 visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
                 modifier = Modifier.fillMaxWidth(0.9f)
             )
-            Row (
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 18.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Row (
-                    modifier = Modifier.padding(end = 75.dp)
-                ){
-                    Image(
-                        painter = painterResource(id = R.drawable.icon_check_circle),
-                        contentDescription = "Checkbox",
-                        modifier = Modifier
-                            .padding(end = 4.dp)
-                    )
 
-                    Text(
-                        text = "Remember me",
-                        fontSize = 16.sp,
-                    )
-                }
+            Spacer(modifier = Modifier.height(12.dp))
 
-                Text(
-                    text = "Forgot password",
-                    fontSize = 16.sp,
-                    color = Color(0xFF03045E)
-                )
-            }
+            // Ô nhập Mật khẩu (Nhập lại mật khẩu)
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                leadingIcon = { Icon(Icons.Default.Lock, contentDescription = "Password Icon") },
+                singleLine = true,
+                shape = RoundedCornerShape(20.dp),
+                //Jetpack Compose Material3
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    containerColor = Color.White
+                ),
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth(0.9f)
+            )
+
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Nút Đăng nhập
             Button(
                 onClick = { /* Xử lý đăng nhập */ },
                 modifier = Modifier
                     .fillMaxWidth(0.6f)
                     .height(50.dp),
                 shape = RoundedCornerShape(25.dp),
-                colors = ButtonDefaults.buttonColors(Color(0xFF023E8A)) // Màu xanh dương đậm
+                colors = ButtonDefaults.buttonColors(Color(0xFF023E8A))
             ) {
-                Text(text = "Sign In", fontSize = 18.sp, color = Color.White)
+                Text(text = "Sign Up", fontSize = 18.sp, color = Color.White)
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Nút Chuyển tiếp
+            Button(
+                onClick = { /* Xử lý đăng nhập */ },
+                modifier = Modifier
+                    .fillMaxWidth(0.2f)
+                    .height(60.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFF023E8A)),
+                shape = RoundedCornerShape(25.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.image_button_back),
+                    contentDescription = "Next",
+                    modifier = Modifier
+                        .size(50.dp)
+                        .fillMaxSize(),
+                )
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview (showBackground = true)
 @Composable
-fun previewSignInLayout (){
-    com.example.appchat_tbdd.ui_layout.Sign_up_layout()
+fun Preview_Sign_up_layout() {
+    Sign_up_layout()
 }
